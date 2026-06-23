710d3066700dbbfa7257b7f1a2d36fbc9b65b6a4
kerolos119 <kerolosbotros99@gmail.com>
2026-06-24 00:59:27 +0300

    database and dashboard

---
+++ database/moch_data.py
@@ -0,0 +1,43 @@
+import psycopg2
+import json
+import random
+from datetime import datetime, timedelta
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    db="zerotouch",
+    user="zerotouch",
+    password="zerotouch"
+)
+cur = conn.cursor()
+
+anomaly_types = ["memory_leak", "cpu_spike", "high_error_rate", "high_latency"]
+actions = ["restart_container", "get_logs", "clear_cache"]
+
+for i in range(20):
+    anomaly_type = random.choice(anomaly_types)
+    cur.execute("""
+                INSERT INTO incidents
+                (timestamp, anomaly_type, anomaly_score, metrics, action_taken, reasoning, resolution, resolved)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, (
+                    datetime.now() - timedelta(minutes=random.randint(1, 120)),
+                    anomaly_types,
+                    round(random.uniform(0.7, 0.99), 4),
+                    json.dumps({
+                        "cpu_mean": random.uniform(20, 95),
+                        "memory_mean": random.uniform(30, 95),
+                        "letancy_p95": random.uniform(0.05, 3.0),
+                        "error_rate": random.uniform(0, 50)
+                    }),
+                    random.choice(actions),
+                    f"Detected {anomaly_type}. Retrieved runbook and executed remediation automatically.",
+                    round(random.uniform(5, 90), 2),
+                    True
+                ))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    print("20 mock incidents inserted,")
